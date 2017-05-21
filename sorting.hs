38def8ab48d30fa54624353f8bd07c4045084d7d
power10dan <lintzu@onid.oregonstate.edu>
2017-05-20 17:54:18 -0700

    Added BubbleSort

--- sorting.hs
+++ sorting.hs
@@ -5,7 +5,7 @@ quicksort (x:xs) =
 	let smallsort = quicksort [a | a<-xs, a <= x]
 	    bigsort = quicksort[a | a<-xs, a > x]
 	 in smallsort ++ [x] ++ bigsort
-	 
+
 -- merge sort 
 merge:: (Ord a)=> [a]->[a]->[a]
 merge xs [] = xs 
@@ -23,8 +23,19 @@ mergesort' list
      | (length list) > 1 = merge (mergesort' ls) (mergesort' rs)
      | otherwise = list 
      where (ls, rs) = splithalf' list 
+     
+-- bubblesort 
+bubblesort :: (Ord a) => [a] -> [a]
+bubblesort [] = []
+bubblesort [x] = [x]
+bubblesort (x:y:xs)
+    | (x > y) = y:bubblesort (x:xs) 
+    | otherwise = x:bubblesort (y:xs) 
 
-
-
+bubblesort' :: (Ord a) => [a] -> Int -> [a]
+bubblesort' xs i 
+    | i == (length xs) = xs
+    | otherwise = bubblesort' (bubblesort xs) (i + 1) 
+ 
 
 
